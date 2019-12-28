c837ece70702d3e05a881dae050da5ea51b132d9
kcinnoy <yonnick00@gmail.com>
2019-12-28 22:18:59 +0000

    face elements -face

--- d3_face/index.js
+++ d3_face/index.js
@@ -1,6 +1,19 @@
 // import {select} from 'd3';
-
 // const svg = select('svg')
-const svg = d3.select('svg')
 
-svg.style('background-color', 'red')
+const svg = d3.select('svg')
+const height = +svg.attr('height');
+const width = +svg.attr('width');
+
+const circle = svg.append('circle')
+  .attr('r', height / 2)
+  .attr('cy', height / 2)
+  .attr('cx', width / 2)
+  .attr('fill', 'yellow')
+  .attr('stroke', 'black')
+
+const leftEye = svg.append('circle')
+  .attr('r', 10)
+  .attr('cy', height / 2)
+  .attr('cx', width / 2)
+  .attr('fill', 'black')
