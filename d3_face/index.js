a7dd48da6b5e68972baf03479cda4ae877de8a0b
kcinnoy <yonnick00@gmail.com>
2019-12-28 23:20:55 +0000

    face elements - eyes and mouth

--- d3_face/index.js
+++ d3_face/index.js
@@ -5,15 +5,32 @@ const svg = d3.select('svg')
 const height = +svg.attr('height');
 const width = +svg.attr('width');
 
-const circle = svg.append('circle')
+const g = svg.append('g')
+  .attr('transform', `translate(${width/2}, ${height/2})`);
+
+const circle = g.append('circle')
   .attr('r', height / 2)
-  .attr('cy', height / 2)
-  .attr('cx', width / 2)
+  // .attr('cy', height / 2)
+  // .attr('cx', width / 2)
   .attr('fill', 'yellow')
   .attr('stroke', 'black')
 
-const leftEye = svg.append('circle')
-  .attr('r', 10)
-  .attr('cy', height / 2)
-  .attr('cx', width / 2)
-  .attr('fill', 'black')
+const leftEye = g.append('circle')
+  .attr('r', 30)
+  .attr('cy', -70)
+  .attr('cx', -100)
+  .attr('fill', 'black');
+
+const rightEye = g.append('circle')
+  .attr('r', 30)
+  .attr('cy', -70)
+  .attr('cx', 100)
+  .attr('fill', 'black');
+
+const mouth = g.append('path')
+  .attr('d', d3.arc()({
+    innerRadius: 0,
+    outerRadius: 170,
+    startAngle: Math.PI / 2,
+    endAngle: Math.PI * 3 /2
+  }))
