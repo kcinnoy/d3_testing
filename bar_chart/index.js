432d76b04e92e06528807a3e888ed1bac58f854b
kcinnoy <yonnick00@gmail.com>
2020-01-01 21:13:12 +0000

    added y scale

--- bar_chart/index.js
+++ bar_chart/index.js
@@ -6,14 +6,22 @@ const height = +svg.attr('height');
 const width = +svg.attr('width');
 
 const render = data => {
+
   const xScale = d3.scaleLinear()
     .domain([0, d3.max(data, d => d.population)])
     .range([0,width]);
+  
+  const yScale = d3.scaleBand()
+    .domain(data.map(d => d.country))
+    .range([0, height]);
+
+  console.log(yScale.domain())
 
   svg.selectAll('rect').data(data)
     .enter().append('rect')
+      .attr('y', d => yScale(d.country))
       .attr('width', d => xScale(d.population))
-      .attr('height', 300)
+      .attr('height', yScale.bandwidth());
 };
 
 d3.csv('data.csv').then(data => {
