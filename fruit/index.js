1a4b1a38e1f4459b4c1f52757e5b1f6b04432361
kcinnoy <yonnick00@gmail.com>
2020-01-26 23:08:46 +0000

    added data bind

--- fruit/index.js
+++ fruit/index.js
@@ -6,3 +6,9 @@ const width = +svg.attr('width');
 const makeFruit = type =>({type});
 
 const fruits = d3.range(5).map(() => makeFruit('apple'));
+
+svg.selectAll('circle').data(fruits)
+  .enter().append('circle')
+    .attr('cx', (d, i) => i * 100)
+    .attr('cy', height / 2)
+    .attr('r', 50);
