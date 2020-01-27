77eab47818c95074b60fc0f48dda04fd132b725f
kcinnoy <yonnick00@gmail.com>
2020-01-27 23:01:47 +0000

    format dom elements

--- fruit/index.js
+++ fruit/index.js
@@ -9,6 +9,7 @@ const fruits = d3.range(5).map(() => makeFruit('apple'));
 
 svg.selectAll('circle').data(fruits)
   .enter().append('circle')
-    .attr('cx', (d, i) => i * 100)
+    .attr('cx', (d, i) => i * 120 + 60)
     .attr('cy', height / 2)
-    .attr('r', 50);
+    .attr('r', 50)
+    .attr('fill', '#d42313');
