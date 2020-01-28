22b37ab15cfa6f2ebbb62c76bd0e55ecb8771349
kcinnoy <yonnick00@gmail.com>
2020-01-28 22:22:59 +0000

    added settime to demo update pattern

--- fruit/index.js
+++ fruit/index.js
@@ -3,13 +3,29 @@ const svg = d3.select('svg')
 const height = +svg.attr('height');
 const width = +svg.attr('width');
 
-const makeFruit = type =>({type});
+const render = (selection, {fruits}) => {
+  selection.selectAll('circle').data(fruits)
+    .enter().append('circle')
+      .attr('cx', (d, i) => i * 120 + 60)
+      .attr('cy', height / 2)
+      .attr('r', 50)
+      .attr('fill', '#d42313');
 
+  selection.selectAll('circle').data(fruits)
+    .exit().remove();
+}
+
+const makeFruit = type =>({type});
 const fruits = d3.range(5).map(() => makeFruit('apple'));
 
-svg.selectAll('circle').data(fruits)
-  .enter().append('circle')
-    .attr('cx', (d, i) => i * 120 + 60)
-    .attr('cy', height / 2)
-    .attr('r', 50)
-    .attr('fill', '#d42313');
+
+render(svg, {fruits});
+
+setTimeout(() => {
+  fruits.pop();
+  render(svg, {fruits});
+}, 1000);
+
+
+
+
