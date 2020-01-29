dcd30324f0fe4f964f4f96cf6258c241c267c9db
kcinnoy <yonnick00@gmail.com>
2020-01-29 23:43:02 +0000

    added ordinal scales based on type

--- fruit/index.js
+++ fruit/index.js
@@ -3,13 +3,21 @@ const svg = d3.select('svg')
 const height = +svg.attr('height');
 const width = +svg.attr('width');
 
+const colorScale = d3.ordinalScale()
+  .domain(['apple', 'lemon'])
+  .range('#c11d1d', 'yellow');
+  
+const radiusScale = d3.ordinalScale()
+  .domain(['apple', 'lemon'])
+  .range(50, 30);
+
 const render = (selection, {fruits}) => {
   selection.selectAll('circle').data(fruits)
     .enter().append('circle')
       .attr('cx', (d, i) => i * 120 + 60)
       .attr('cy', height / 2)
       .attr('r', 50)
-      .attr('fill', '#d42313');
+      .attr('fill', d => colorScale(d.type));
 
   selection.selectAll('circle').data(fruits)
     .exit().remove();
@@ -23,6 +31,11 @@ render(svg, {fruits});
 
 setTimeout(() => {
   fruits.pop();
+  render(svg, {fruits});
+}, 1000);
+
+setTimeout(() => {
+  fruits[2].tyope = 'lemon';
   render(svg, {fruits});
 }, 1000);
 
