6a97fd318c9c050133488c8d7608786eb5504bdd
kcinnoy <yonnick00@gmail.com>
2020-01-30 20:44:25 +0000

    update update method

--- fruit/index.js
+++ fruit/index.js
@@ -3,24 +3,26 @@ const svg = d3.select('svg')
 const height = +svg.attr('height');
 const width = +svg.attr('width');
 
-const colorScale = d3.ordinalScale()
+const colorScale = d3.scaleOrdinal()
   .domain(['apple', 'lemon'])
-  .range('#c11d1d', 'yellow');
+  .range(['#c11d1d', 'yellow']);
   
-const radiusScale = d3.ordinalScale()
+const radiusScale = d3.scaleOrdinal()
   .domain(['apple', 'lemon'])
-  .range(50, 30);
+  .range([50, 30]);
 
 const render = (selection, {fruits}) => {
-  selection.selectAll('circle').data(fruits)
-    .enter().append('circle')
+  const circles = selection.selectAll('circle').data(fruits)
+    
+  circles.enter().append('circle')
       .attr('cx', (d, i) => i * 120 + 60)
       .attr('cy', height / 2)
-      .attr('r', 50)
+      .attr('r', d => radiusScale(d.type))
       .attr('fill', d => colorScale(d.type));
-
-  selection.selectAll('circle').data(fruits)
-    .exit().remove();
+  circles
+      .attr('r', d => radiusScale(d.type))
+      .attr('fill', d => colorScale(d.type));
+  circles.exit().remove();
 }
 
 const makeFruit = type =>({type});
@@ -35,9 +37,9 @@ setTimeout(() => {
 }, 1000);
 
 setTimeout(() => {
-  fruits[2].tyope = 'lemon';
+  fruits[2].type = 'lemon';
   render(svg, {fruits});
-}, 1000);
+}, 2000);
 
 
 
