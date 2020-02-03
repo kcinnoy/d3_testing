6232793f89c150eb97c58b3a583953e0dfbac8d3
kcinnoy <yonnick00@gmail.com>
2020-02-03 23:38:45 +0000

    replace duplicated code with functions

--- fruit/index.js
+++ fruit/index.js
@@ -11,17 +11,19 @@ const radiusScale = d3.scaleOrdinal()
   .domain(['apple', 'lemon'])
   .range([50, 30]);
 
+const xPosition = (d, i) => i * 120 + 60
+
 const render = (selection, {fruits}) => {
   const circles = selection.selectAll('circle').data(fruits, d => d.id);
     
   circles.enter().append('circle') 
-      .attr('cx', (d, i) => i * 120 + 60)    
+      .attr('cx', xPosition)    
       .attr('cy', height / 2)
       .attr('r', 0)
     .merge(circles)
       .attr('fill', d => colorScale(d.type))
     .transition().duration(1000)
-      .attr('cx', (d, i) => i * 120 + 60)
+      .attr('cx', xPosition)
       .attr('r', d => radiusScale(d.type))   
   circles.exit()
     .transition().duration(1000)
